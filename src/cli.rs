6a06aaf300bcea13899105509718cb897f77d3be
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2022-06-27 14:56:35 +0200

    try rewriting the parser in pest
    On the one hand, it kind of looks like it's a relatively powerful
    parser, and it does what I want without too much pain, so that's good.
    On the other hand, that's a whole lot of `.unwrap()` in the example!
    Need to do some research to ensure that we're doing it right.

--- src/cli.rs
+++ src/cli.rs
@@ -5,8 +5,11 @@
 
 use chrono::{DateTime, Duration, NaiveDate, Utc};
 use chrono_english::Interval;
+use pest::Parser;
 
-lalrpop_mod!(pub cli_parser);
+#[derive(pest_derive::Parser)]
+#[grammar = "cli_parser.pest"]
+pub struct CliParser;
 
 pub struct BareMessage {
     message: String,
@@ -43,4 +46,43 @@ pub enum Cli {
     //     to: NaiveDate,
     //     time_tracking: bool,
     // },
+}
+
+impl Cli {
+    fn parse(input: &str) -> Result<Self, Error> {
+        use pest::iterators::Pair;
+
+        let cli = CliParser::parse(Rule::cli_parser, input)?
+            .next()
+            .expect("a good match should return Some");
+
+        match cli.as_rule() {
+            Rule::start => todo!(),
+            Rule::stop => todo!(),
+            Rule::started => todo!(),
+            Rule::stopped => todo!(),
+            Rule::started_at => todo!(),
+            Rule::stopped_at => todo!(),
+            Rule::report => todo!(),
+            Rule::report_for => todo!(),
+            Rule::EOI
+            | Rule::WHITESPACE
+            | Rule::cli_parser
+            | Rule::time_tracking
+            | Rule::message
+            | Rule::time_spec
+            | Rule::interval
+            | Rule::bare_message
+            | Rule::relative_message
+            | Rule::absolute_message => {
+                unreachable!("cannot arrive at these rules from the top-level cli")
+            }
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("parse error")]
+    Parse(#[from] pest::error::Error<Rule>),
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
-#[macro_use]
-extern crate lalrpop_util;
-
 mod cli;
 
 fn main() -> color_eyre::eyre::Result<()> {
     color_eyre::install()?;
 
-    let opts = Cli::parse();
-    match opts.subcommand {
-        Command::Paths {
-            which: PathSubcommand::Config,
-        } => {
-            println!("{}", worklog::paths::config().display());
-        }
-        Command::Paths {
-            which: PathSubcommand::Database,
-        } => {
-            println!("{}", worklog::paths::database().display());
-        }
-        other => {
-            eprintln!("not yet supported:\n{:?}", other);
-            panic!("unsupported subcommand");
-        }
-    }
+    todo!("do something with the cli");
+    // let opts = Cli::parse();
+    // match opts.subcommand {
+    //     Command::Paths {
+    //         which: PathSubcommand::Config,
+    //     } => {
+    //         println!("{}", worklog::paths::config().display());
+    //     }
+    //     Command::Paths {
+    //         which: PathSubcommand::Database,
+    //     } => {
+    //         println!("{}", worklog::paths::database().display());
+    //     }
+    //     other => {
+    //         eprintln!("not yet supported:\n{:?}", other);
+    //         panic!("unsupported subcommand");
+    //     }
+    // }
 
     Ok(())
 }
